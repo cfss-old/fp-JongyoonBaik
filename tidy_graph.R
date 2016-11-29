dde9a0c4563f93d7226aa5f26aa3f2f2d12bdb18
Jongyoon <baikjongyoon@uchicago.edu>
2016-11-28 19:58:47 -0600

    map trouble

--- tidy_graph.R
+++ tidy_graph.R
@@ -4,9 +4,22 @@ library(readr)
 library(forcats)
 library(feather)
 
-
+##modify tables
 litigation_china <- read_csv("data.csv") %>%
   tbl_df()
+location_china <- read_csv("location.csv") %>%
+  tbl_df()
+
+###average litigation cases
+litigation_average <- litigation_china %>%
+  group_by(province) %>%
+  summarise(
+    average_litigation = mean(cases)
+  )
+
+###merging two data
+litigation_location <- litigation_average %>%
+  full_join(location_china, by = "province")
 
 ##create a map
 library(ggmap)
@@ -18,3 +31,12 @@ china_map <-ggmap(
     zoom = 4)
 )
 ggsave("results/china_map.png", china_map)
+
+###express the data on the map
+china_map +
+  geom_point(data = litigation_location,
+             aes(x = long, y = lat),
+             alpha = .3,
+             color = "red") +
+  ggtitle("Average Number of Cases")
+
